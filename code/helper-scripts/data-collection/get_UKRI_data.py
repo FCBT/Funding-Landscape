52f21f8aef72231089070aef46ebbc5884ed707a
Flavia Trigo <flaviacbtrigo@gmail.com>
2022-09-14 14:46:20 +0100

    updated code

--- code/helper-scripts/data-collection/get_UKRI_data.py
+++ code/helper-scripts/data-collection/get_UKRI_data.py
@@ -6,12 +6,13 @@ import time
 import os
 
 ## load csv file downloaded from URKI website
-project_metadata = pd.read_csv("./raw-data/fine-scale/UK/UKRI/UKRI_raw_metadata.csv")
+project_metadata = pd.read_csv("/Users/flavia/Library/CloudStorage/OneDrive-ImperialCollegeLondon/Funding-Landscape/raw-data/fine-scale/UK/UKRI/UKRI_raw_metadata.csv")
 print("data loaded")
+data = project_metadata
 # filter projects by research grant and fellowships
-data = project_metadata[project_metadata.ProjectCategory.isin(["Research Grant", "Fellowship"])]
+# data = project_metadata[project_metadata.ProjectCategory.isin(["Research Grant", "Fellowship"])]
 #filter STEM funding bodies
-stem = ["BBSRC","EPSRC", "NERC", "STFC", "MRC"]
+stem = ["BBSRC","EPSRC", "NERC", "STFC", "MRC", "Innovate UK"]
 data = data[data.FundingOrgName.isin(stem)]
 
 print('data filtered')
@@ -24,14 +25,14 @@ headers = {'Accept':'application/vnd.rcuk.gtr.json-v7'}
 
 for ind in data.index:
     #check if file exists to prevent extra work
-    if (data["ProjectId"][ind]+'.txt') not in os.listdir('./raw-data/fine-scale/UK/UKRI/titles-abstracts/'):
+    if (data["ProjectId"][ind]+'.txt') not in os.listdir('/Users/flavia/Library/CloudStorage/OneDrive-ImperialCollegeLondon/Funding-Landscape/raw-data/fine-scale/UK/UKRI/titles-abstracts/'):
         print(ind)
         #request data
         r = requests.get("https://gtr.ukri.org/gtr/api/projects/" + data["ProjectId"][ind], headers = headers)
          #if request is successful
         if r.status_code == 200:
             json_r = r.json()
-            with open('./raw-data/fine-scale/UK/UKRI/titles-abstracts/'+ data["ProjectId"][ind]+'.txt', 'w') as f:
+            with open('/Users/flavia/Library/CloudStorage/OneDrive-ImperialCollegeLondon/Funding-Landscape/raw-data/fine-scale/UK/UKRI/titles-abstracts/'+ data["ProjectId"][ind]+'.txt', 'w') as f:
                 f.write(json_r["title"] + " " + json_r["abstractText"])
             
             time.sleep(0.5)    
