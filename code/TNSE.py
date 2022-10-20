81b1cd3e7bc60e856ddd87de870f68a75defdddc
Flavia Trigo <flaviacbtrigo@gmail.com>
2022-10-20 13:43:45 +0200

    update TSNE

--- code/TNSE.py
+++ code/TNSE.py
@@ -1,1 +1,16 @@
 from MulticoreTSNE import MulticoreTSNE as TSNE
+import numpy as np
+
+#read doc_topic_matrix
+with open("./results/fine-scale/mallet-models/UK/800-topics-doc.txt","r") as f:
+        txt = f.read().splitlines()
+
+#parse probabilties
+for (i,v) in enumerate(txt):
+    txt[i] = [float(x) for x in v.split()[2:]]
+#turn into numpy array
+probs = np.array(txt)
+
+probs.shape
+
+tsne_fit = TSNE(n_jobs=4, verbose=1).fit_transform(probs)
